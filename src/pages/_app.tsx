f961ce283ef01e2b4383904c53ba6f403916a509
evfjunior <evfjunior@protonmail.com>
2022-05-24 10:04:19 -0300

    chore: add links and meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" href="/assets/icon-512.png" />
+        <link rel="icon" href="/assets/icon-128.png" />
+        <link rel="shortcut" href="/assets/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A complete project structure to create React web applications using NextJS and Typescript"
+        />
+        <title>NextJS Template</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
