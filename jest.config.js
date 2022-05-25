e900bc3cc6ab51b43f59a954b0ee28e0baa49b18
evfjunior <evfjunior@protonmail.com>
2022-05-25 01:12:51 -0300

    chore: add and configure jest and testing-library

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: '.'
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/mock.ts',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/types/**/*.d.ts',
+    '!src/utils/**/*.ts(x)'
+  ]
+}
+
+module.exports = createJestConfig(customJestConfig)
