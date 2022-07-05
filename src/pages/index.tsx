d00c589e88e3b968e2ffcbdedd45fb55b6e5fc53
evfjunior <evfjunior@protonmail.com>
2022-07-04 22:45:18 -0300

    fix(layout): remove layout component from _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
+import { Layout } from '@components/Layout'
+
 export default function Index() {
   return (
-    <article>
-      <h2>NextJS Template</h2>
-      <p>
-        A complete project structure to create React web applications using
-        NextJS and Typescript
-      </p>
-    </article>
+    <Layout>
+      <article>
+        <h2>NextJS Template</h2>
+        <p>
+          A complete project structure to create React web applications using
+          NextJS and Typescript
+        </p>
+      </article>
+    </Layout>
   )
 }
